9e4828e2c461647b6b5e70f49f8d469abb0b6ee0
Patrick Ly <patrickkimly@gmail.com>
2019-08-30 20:54:28 -0700

    Change character images

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,11 +46,30 @@ var i = 0;
 while (i < pokemonsEl.length) {
 	pokemonsEl[i].onclick = function() {
 		var pokemonName = this.dataset.pokemon;
+		var player1Img = document
+			.querySelector('.player1')
+			.getElementsByTagName('img');
+		var player2Img = document
+			.querySelector('.player2')
+			.getElementsByTagName('img');
+
 		gameState.userPokemon = pokemonName;
 
 		cpuPick();
 		battleScreenEl.classList.toggle('active');
-		console.log(gameState);
+
+		var currentPokemon = pokemonDB.filter(function(pokemon) {
+			return pokemon.name == gameState.userPokemon;
+		});
+
+		var currentRivalPokemon = pokemonDB.filter(function(pokemon) {
+			return pokemon.name == gameState.rivalPokemon;
+		});
+
+		player1Img[0].src = currentPokemon[0].img;
+		player2Img[0].src = currentRivalPokemon[0].img;
+
+		console.log(currentPokemon);
 	};
 	i++;
 }
